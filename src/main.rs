c1760f8d169133179e043582a7dd422c17152ff4
Leandro Braga <leandrobbraga@gmail.com>
2022-09-18 16:15:12 -0300

    Introduce automatic calculation of the amount of parallelism available in the host machine.

--- src/main.rs
+++ src/main.rs
@@ -6,18 +6,19 @@ use std::{fmt::Display, ops::AddAssign, thread};
 use game::Game;
 use player::{Player, RandomPlayer};
 
-static CORES: usize = 24;
-static GAME_COUNT: u64 = 1_000_000_000;
+static GAME_COUNT: u64 = 1_000;
 
 struct GamesResult {
     victories: u64,
     draws: u64,
+    losses: u64,
 }
 
 impl AddAssign for GamesResult {
     fn add_assign(&mut self, rhs: Self) {
         self.victories += rhs.victories;
         self.draws += rhs.draws;
+        self.losses += rhs.losses;
     }
 }
 
@@ -40,13 +41,23 @@ fn main() {
     let mut games_results = GamesResult {
         victories: 0,
         draws: 0,
+        losses: 0,
     };
+    let available_parallelism = std::thread::available_parallelism().unwrap().get();
 
     thread::scope(|s| {
-        let mut handlers = Vec::with_capacity(CORES);
+        let mut handlers = Vec::with_capacity(available_parallelism);
 
-        for _ in 0..CORES {
-            handlers.push(s.spawn(|| play_games(&player_1, &player_2, GAME_COUNT as usize / CORES)))
+        for _ in 0..available_parallelism {
+            // NOTE: This code is not correct because it just truncates the division result,
+            // but it's fine for this application.
+            handlers.push(s.spawn(|| {
+                play_games(
+                    &player_1,
+                    &player_2,
+                    GAME_COUNT as usize / available_parallelism,
+                )
+            }))
         }
 
         for handler in handlers {
@@ -64,9 +75,8 @@ where
 {
     let mut victories = 0;
     let mut draws = 0;
+    let mut losses = 0;
 
-    // NOTE: This code is not correct because it just truncates the division result,
-    // but it's fine for this application.
     for _ in 0..n {
         let winner = play_game(player_1, player_2);
 
@@ -77,11 +87,17 @@ where
             None => {
                 draws += 1;
             }
-            _ => (),
+            _ => {
+                losses += 1;
+            }
         };
     }
 
-    GamesResult { victories, draws }
+    GamesResult {
+        victories,
+        draws,
+        losses,
+    }
 }
 
 fn play_game<T, U>(player_1: &T, player_2: &U) -> Option<game::Player>
@@ -98,7 +114,7 @@ where
         let player = &players[player_idx % 2];
 
         let action = player.play(&game.board, game.available_actions());
-        
+
         if game.act(action).is_ok() {
             player_idx += 1;
         };
